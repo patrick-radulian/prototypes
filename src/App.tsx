493783181795dfc64dfcbeb965de7027adb20305
Patrick Radulian <patrick.radulian@gmail.com>
2022-02-03 11:07:59 +0100

    Separate header component and remove grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
-import { CssBaseline, Grid, IconButton, InputAdornment, TextField } from '@mui/material';
-import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
-import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
-import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import { CssBaseline } from '@mui/material';
+import Header from 'components/header/header';
+
 
 function App() {
     return (
         <CssBaseline>
-            <Grid container>
-                <Grid container item xs={12}>
-                    <Grid container item xs={3} sx={{background: "#00ADEE"}}>
-                        <Grid item justifyContent="center" alignItems="center">
-                            <IconButton><MenuRoundedIcon/></IconButton>
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={6} sx={{p: 1, background: "lightblue"}}>
-                        <TextField label="Search" size="small" InputProps={{endAdornment: (<InputAdornment position="start"><SearchRoundedIcon/></InputAdornment>)}} fullWidth/>
-                    </Grid>
-                    <Grid item xs={3} sx={{background: "#00ADEE"}}>
-                        <Grid item justifyContent="center" alignItems="center">
-                            <IconButton><AccountCircleRoundedIcon/></IconButton>
-                        </Grid>
-                    </Grid>
-                </Grid>
-                <Grid item xs={12}>Row B</Grid>
-            </Grid>
+            <Header/>
         </CssBaseline>
     );
 }
---
+++ src/components/header/header.tsx
@@ -0,0 +1,26 @@
+import { Grid, IconButton, InputAdornment, TextField } from "@mui/material";
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+
+function Header() {
+    return (
+        <Grid container item xs={12}>
+            <Grid container item xs={3} sx={{background: "#00ADEE"}}>
+                <Grid item justifyContent="center" alignItems="center">
+                    <IconButton><MenuRoundedIcon/></IconButton>
+                </Grid>
+            </Grid>
+            <Grid item xs={6} sx={{p: 1, background: "lightblue"}}>
+                <TextField label="Search" size="small" InputProps={{endAdornment: (<InputAdornment position="start"><SearchRoundedIcon/></InputAdornment>)}} fullWidth/>
+            </Grid>
+            <Grid item xs={3} sx={{background: "#00ADEE"}}>
+                <Grid item justifyContent="center" alignItems="center">
+                    <IconButton><AccountCircleRoundedIcon/></IconButton>
+                </Grid>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default Header;
